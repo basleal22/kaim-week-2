2189e19cdb947021a0baa92fb81932c4f222cd21
basleal22 <solibaslu@gmail.com>
2024-12-20 14:58:44 +0300

    added data segment function based on decile class

--- scripts/aggregation.py
+++ scripts/aggregation.py
@@ -23,3 +23,13 @@ def data_aggregation(data):
     aggregated_data['total_upload']=aggregated_data[['total_youtube_ul','total_social_media_ul','total_gaming_ul','total_email_ul','total_google_ul','total_other_ul']].sum(axis=1)
     aggregated_data['total_data_volume']=aggregated_data[['total_download','total_upload']].sum(axis=1)
     return aggregated_data
+def segment_data(data):
+    #sort the data by session duration
+    aggregated_data=data.sort_values(by='session_duration',ascending=False)
+    #create a new column for decile class
+    aggregated_data['decile_class']=pd.qcut(aggregated_data['session_duration'],10,label=False)+1#segments the users into 10 equal sized decile classes
+    #calculate total volume of upload and download for each class and create a new column for it
+    aggregated_data['total_data']=aggregated_data[['total_download','total_upload']].sum(axis=1)#sum(axis=1) because axis =1 is for rows
+    #group by decile class and calculate the total data per class
+    aggregated_data=aggregated_data.groupby('decile_class').agg(total_data_per_decile=('total_data','sum')).reset_index()
+    return aggregated_data
