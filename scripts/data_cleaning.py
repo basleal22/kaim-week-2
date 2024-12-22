132770176e8f526557fc97a3d710627e1d9f71bd
basleal22 <solibaslu@gmail.com>
2024-12-22 09:36:38 +0300

    added data cleaning function

---
+++ scripts/data_cleaning.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+def data_extraction(data):
+    data = pd.read_csv(data)
+#def data_cleaning(data):
+    #for columns in data[columns]:
+        #if data['data.isnull().sum>0
+        #cleaned_data=
